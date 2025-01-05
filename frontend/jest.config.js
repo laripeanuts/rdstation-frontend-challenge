9579a196740c5fca5c25bfa4c058d25f6a2768cd
Larissa Rabelo <larissarabelolf@gmail.com>
2025-01-05 19:56:58 -0300

    chore: config jest

---
+++ frontend/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|scss|sass)$': 'identity-obj-proxy',
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+};
